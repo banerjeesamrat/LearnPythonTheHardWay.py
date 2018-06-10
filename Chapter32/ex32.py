6b28177d77ed58beb86ec2a34aaffd9a3ff58fcf
banerjeesamrat <banerjeesamrat3@gmail.com>
2018-06-10 21:33:39 +0530

    Getting back on track

--- Chapter32/ex32.py
+++ Chapter32/ex32.py
@@ -1,7 +1,37 @@
 """
 
 Author: Samrat Banerjee
-Date: 05-06-2018
+Date: 06-06-2018
 Description: Loops and Lists
 
 """
+
+the_count=[1,2,3,4,5]
+fruits=['apples','oranges','pears','apricots']
+change=[1,'pennies',2,'dimes',3,'quarters']
+
+# the first kind of for-loop goes through a list
+
+for number in the_count:
+    print "This is count %d" %number
+
+for fruit in fruits:
+    print "A fruit of type: %s" %fruit
+
+# also we can go through mixed lists also
+# notice we have to use %r since we don't know what's in it
+for i in change:
+    print "I got %r" %i
+
+# we can also build lists, first start with an empty one
+elements=[]
+
+# then use the range function to do 0 to 5 counts
+for i in range(0,6):
+    print "Adding %d to the list" %i
+    # append is a function that lists understand
+    elements.append(i)
+
+# now we can print them out too
+for i in elements:
+    print "Element was: %d" %i
